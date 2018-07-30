16ea2d7852e79747be15f8cbb1f72423a7d24aa7
singletonn <sopranowest@gmail.com>
2018-07-30 22:02:30 +1000

    В каждом диалоге получаем количество не прочитанных сообщений

--- api/models/MessengerThreads.php
+++ api/models/MessengerThreads.php
@@ -2,7 +2,9 @@
 
 namespace pantera\messenger\api\models;
 
+use pantera\messenger\models\MessengerViewed;
 use Yii;
+use yii\db\Expression;
 
 /**
  * Class MessengerThreads
@@ -18,9 +20,34 @@ class MessengerThreads extends \pantera\messenger\models\MessengerThreads
             'id',
             'subject',
             'lastMessage',
+            'countNotViewed',
         ];
     }
 
+    /**
+     * Получить количество не прочитанных сообщений в диалоге
+     * @return int
+     */
+    public function getCountNotViewed()
+    {
+        return MessengerMessages::find()
+            ->joinWith(['thread'])
+            ->leftJoin(MessengerViewed::tableName(), [
+                'AND',
+                ['=', MessengerViewed::tableName() . '.message_id', new Expression(MessengerMessages::tableName() . '.id')],
+                ['=', MessengerViewed::tableName() . '.user_id', Yii::$app->user->id]
+            ])
+            ->andWhere(['!=', MessengerMessages::tableName() . '.user_id', Yii::$app->user->id])
+            ->andWhere(['=', MessengerMessages::tableName() . '.thread_id', $this->id])
+            ->andWhere(['IS', MessengerViewed::tableName() . '.message_id', null])
+            ->count();
+    }
+
+    /**
+     * Получить последние сообщение в диалоге
+     * @return \yii\db\ActiveQuery
+     * @throws \yii\base\InvalidConfigException
+     */
     public function getLastMessage()
     {
         $object = Yii::createObject(\pantera\messenger\models\MessengerMessages::className());
