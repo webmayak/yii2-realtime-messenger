a669280a249606bbe47e33a5def772ee12397cec
singletonn <sopranowest@gmail.com>
2018-09-12 12:27:13 +1000

    fix

--- migrations/m180728_134842_update_tables_name.php
+++ migrations/m180728_134842_update_tables_name.php
@@ -12,10 +12,17 @@ class m180728_134842_update_tables_name extends Migration
      */
     public function safeUp()
     {
-        $this->renameTable('messages_log', '{{%messages_log}}');
-        $this->renameTable('messenger_messages', '{{%messenger_messages}}');
-        $this->renameTable('messenger_threads', '{{%messenger_threads}}');
-        $this->renameTable('messenger_viewed', '{{%messenger_viewed}}');
+        $this->renameTable('messages_log', 'messages_log_temp');
+        $this->renameTable('messages_log_temp', '{{%messages_log}}');
+
+        $this->renameTable('messenger_messages', 'messenger_messages_temp');
+        $this->renameTable('messenger_messages_temp', '{{%messenger_messages}}');
+
+        $this->renameTable('messenger_threads', 'messenger_threads_temp');
+        $this->renameTable('messenger_threads_temp', '{{%messenger_threads}}');
+
+        $this->renameTable('messenger_viewed', 'messenger_viewed_temp');
+        $this->renameTable('messenger_viewed_temp', '{{%messenger_viewed}}');
     }
 
     /**
@@ -24,10 +31,17 @@ class m180728_134842_update_tables_name extends Migration
     public function safeDown()
     {
         echo "m180728_134842_update_tables_name cannot be reverted.\n";
-        $this->renameTable('{{%message_log}}', 'message_log');
-        $this->renameTable('{{%messenger_messages}}', 'messenger_messages');
-        $this->renameTable('{{%messenger_threads}}', 'messenger_threads');
-        $this->renameTable('{{%messenger_viewed}}', 'messenger_viewed');
+        $this->renameTable('{{%messages_log}}', 'messages_log_temp');
+        $this->renameTable('messages_log_temp', 'messages_log');
+
+        $this->renameTable('{{%messenger_messages}}', 'messenger_messages_temp');
+        $this->renameTable('messenger_messages_temp', 'messenger_messages');
+
+        $this->renameTable('{{%messenger_threads}}', 'messenger_threads_temp');
+        $this->renameTable('messenger_threads_temp', 'messenger_threads');
+
+        $this->renameTable('{{%messenger_viewed}}', 'messenger_viewed_temp');
+        $this->renameTable('messenger_viewed_temp', 'messenger_viewed');
         return true;
     }
 }
